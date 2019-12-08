2326e8b565c27415489d553e4c1c7ea68a77b310
aleksey <ahrebtan@gmail.com>
2019-12-08 21:03:13 +0200

    first commit

---
+++ homework/hral/index.js
@@ -0,0 +1,18 @@
+function watch(message) {
+    return message;
+}
+watch('simple word');
+
+console.log(watch('simple word'));
+
+let color = getColor;
+color();
+
+function getColor(color = 'red') {
+    if (color == 'red') {
+        return;
+    }
+    console.log(color);
+}
+
+color('yellow');
