8012615a724e3b5e53ff322433e7d9afc8a4e3d5
BNastya <breslavnastya@gmail.com>
2019-12-15 16:03:51 +0200

    homework2: added function for printing numbers and for checking if word is polindrom

--- homework/anac/index.js
+++ homework/anac/index.js
@@ -1,16 +1,35 @@
-function sortByAge(arr) {
-  arr.sort((a, b) => a.age > b.age ? 1 : -1);
+function printNumbers () {
+   for (let i = 1; i <= 100; i++) {
+      const remOfDevision1 = i % 3;
+      const remOfDevision2 = i % 5;
+      let resultString = '';
+
+      if (remOfDevision1 === 0) {
+         resultString = 'Fizz';
+      }
+      if (remOfDevision2 === 0) {
+         resultString = resultString + 'Bazz';
+      }
+      if (remOfDevision1 !== 0 && remOfDevision2 !== 0) {
+         resultString = i;
+      }
+      console.log(resultString);
+   }
 }
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+function isPolindrom (word) {
+   const len = word.length;
+   word = word.toUpperCase();
+   for (let i = 1; i <= len / 2; i++) {
+      if (word[i - 1] !== word[len - i]) {
+         return false;
+      }
+   }
+   return true;
+}
 
-let arr = [ vasya, petya, masha ];
-
-sortByAge(arr);
-
-// теперь отсортировано: [vasya, masha, petya]
-alert(arr[0].name); // Вася
-alert(arr[1].name); // Маша
-alert(arr[2].name); // Петя
+printNumbers();
+console.log('absba: ' + isPolindrom('absba'));
+console.log('abssba: ' + isPolindrom('abssba'));
+console.log('abSsBa: ' + isPolindrom('abSsBa'));
+console.log('abS7Ba: ' + isPolindrom('abS7Ba'));
