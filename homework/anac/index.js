1d0bf45ec0e05f97d33fec5586225db403d5a00d
BNastya <breslavnastya@gmail.com>
2019-12-16 20:16:58 +0200

    homework2: corrected according to notices

--- homework/anac/index.js
+++ homework/anac/index.js
@@ -1,35 +1,46 @@
 function printNumbers () {
    for (let i = 1; i <= 100; i++) {
-      const remOfDevision1 = i % 3;
-      const remOfDevision2 = i % 5;
+      const remOfDivision1 = i % 3;
+      const remOfDivision2 = i % 5;
       let resultString = '';
 
-      if (remOfDevision1 === 0) {
+      if (remOfDivision1 === 0) {
          resultString = 'Fizz';
       }
-      if (remOfDevision2 === 0) {
+      if (remOfDivision2 === 0) {
          resultString = resultString + 'Bazz';
       }
-      if (remOfDevision1 !== 0 && remOfDevision2 !== 0) {
+      if (remOfDivision1 !== 0 && remOfDivision2 !== 0) {
          resultString = i;
       }
       console.log(resultString);
    }
 }
 
-function isPolindrom (word) {
-   word = word.toUpperCase().replace(/\s/g, '');
-   const len = word.length;
-   for (let i = 1; i <= len / 2; i++) {
-      if (word[i - 1] !== word[len - i]) {
-         return false;
+function isPalindrom (data) {
+   if (data) {
+      data = data.toString();
+      data = data.toUpperCase().replace(/\s/g, '');
+      const len = data.length;
+
+      for (let i = 1; i <= len / 2; i++) {
+         if (data[i - 1] !== data[len - i]) {
+            return false;
+         }
       }
+      return true;
    }
-   return true;
+   return false;
 }
 
 printNumbers();
-console.log('absba: ' + isPolindrom('absba'));
-console.log('Я иду с мечем судия: ' + isPolindrom('Я иду с мечем судия'));
-console.log('abSsBa: ' + isPolindrom('abSsBa'));
-console.log('abS7Ba: ' + isPolindrom('abS7Ba'));
+console.log('absba: ' + isPalindrom('absba'));
+console.log('Я иду с мечем судия: ' + isPalindrom('Я иду с мечем судия'));
+console.log('abSsBa: ' + isPalindrom('abSsBa'));
+console.log('abS7Ba: ' + isPalindrom('abS7Ba'));
+console.log('[1, 2, 3, 3, 2, 1]: ' + isPalindrom([1, 2, 3, 3, 2, 1]));
+console.log('null: ' + isPalindrom(null));
+console.log('undefined: ' + isPalindrom(undefined));
+console.log(': ' + isPalindrom());
+console.log('Aa : ' + isPalindrom('Aa '));
+console.log('{}:' + isPalindrom({}));
